a8765001ed8896658c0c6b0e221e9bc407e33f48
Marcelino Feliciano de Sousa <marcelino.feliciano@hotmail.com>
2022-11-24 09:57:30 -0400

    Add role attribute

--- src/main/java/br/com/mfsdevsys/domain/User.java
+++ src/main/java/br/com/mfsdevsys/domain/User.java
@@ -3,6 +3,7 @@ package br.com.mfsdevsys.domain;
 import java.util.ArrayList;
 import java.util.List;
 
+import br.com.mfsdevsys.domain.enums.Role;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -16,6 +17,7 @@ public class User {
 	private String name;
 	private String email;
 	private String password;
+	private Role role;
 	private List<Request> requests = new ArrayList<Request>();
 	private List<RequestStage> stages = new ArrayList<RequestStage>();
 	
