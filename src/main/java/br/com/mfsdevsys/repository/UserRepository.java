aa08cbcb6f260950b927cae8207806c07b3ca7b1
Marcelino Feliciano de Sousa <marcelino.feliciano@hotmail.com>
2022-11-24 14:50:11 -0400

    Update User Repository

--- src/main/java/br/com/mfsdevsys/repository/UserRepository.java
+++ src/main/java/br/com/mfsdevsys/repository/UserRepository.java
@@ -4,9 +4,11 @@ import java.util.Optional;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.stereotype.Repository;
 
 import br.com.mfsdevsys.domain.User;
 
+@Repository
 public interface UserRepository extends JpaRepository<User, Long>{
 
 	public User FindByName( String name);
