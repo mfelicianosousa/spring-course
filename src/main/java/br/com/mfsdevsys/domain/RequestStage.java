963c82c8a11f003b388b066ddd5ff22968c39cbd
Marcelino Feliciano de Sousa <marcelino.feliciano@hotmail.com>
2022-11-24 13:48:39 -0400

    Map Entity

--- src/main/java/br/com/mfsdevsys/domain/RequestStage.java
+++ src/main/java/br/com/mfsdevsys/domain/RequestStage.java
@@ -2,8 +2,19 @@ package br.com.mfsdevsys.domain;
 
 import java.util.Date;
 
-import br.com.mfsdevsys.domain.enums.RequestState;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.EnumType;
+import javax.persistence.Enumerated;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.Temporal;
+import javax.persistence.TemporalType;
 
+import br.com.mfsdevsys.domain.enums.RequestState;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -12,14 +23,30 @@ import lombok.Setter;
 @NoArgsConstructor
 @AllArgsConstructor
 @Getter @Setter
+@Entity(name="request_stage")
 public class RequestStage {
 	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
-	private Date realizatonDate;
+	
+	@Column(columnDefinition = "text")
 	private String description;
+	
+	@Column(name= "realization_date", nullable = false)
+	@Temporal(TemporalType.TIMESTAMP)
+	private Date realizatonDate;
+	
+	@Column(length = 12, nullable = false)
+	@Enumerated(EnumType.STRING)
 	private RequestState state;
+	
+	@ManyToOne
+	@JoinColumn(name = "request_id", nullable = false)
 	private Request request;
+	
+	@ManyToOne
+	@JoinColumn(name = "user_id", nullable = false)
 	private User user;
 	
-
 }
